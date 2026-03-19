f0015062dc6f6ed991d6c1fd9a3f2091878c1b5a
eric <eric.moral.7e7@itb.cat>
2026-03-19 13:04:47 +0100

    prueba

---
+++ data/camisetas.js
@@ -0,0 +1,4 @@
+export let camisetas = [
+    { id: 1, nombre: "Ameri", descripcion: "Camiseta del disco Ameri", precioBase: 35, tallas: ["XS","S","M", "L"], colores: ["Azul", "Blanco"], tags: ["Ameri", "Duki"] }, 
+    { id: 2, nombre: "C.R.O", descripcion: "Camiseta de merch de C.R.O", precioBase: 40, tallas: ["XS","S","M"], colores: ["Naranja", "Negro"], tags: ["C.R.O"]}
+];
